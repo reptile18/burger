abffd1ee3b12f5f7ee531821a2a4eb5326e9e886
Johnny Li <reptile18@gmail.com>
2020-06-25 16:39:24 -0700

    directories

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers VALUES 
+(NULL,"Black Bean",FALSE),
+(NULL,"Veggie",FALSE),
+(NULL,"Chick'n",FALSE),
+(NULL,"Impossible",FALSE),
+(NULL,"Beyond",FALSE),
+(NULL,"Improbable",TRUE);
+
+SELECT * FROM burgers;
